bfed33c794ae736386fc2146ce79b5d683af6991
Vladyslav Bazdyrev <vladyslav.bazdyrev@techfabric.com>
2022-10-26 17:27:52 +0300

    dont remember

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,12 @@
-import React, { FC } from 'react';
-import { GAME_LEVELS } from './components/levels/levels';
-import './index.css'
+import React, { FC } from "react";
+import RenderLevel from "./components/RenderLever";
 
-const App:FC = ():JSX.Element => {
-  
-  const level = GAME_LEVELS[0].trim().split('\n');
-
-  // console.log(level[0])
-  
-
+const App: FC = (): JSX.Element => {
   return (
-
     <div className="App">
-      <div>
-      {level.map((row) => {
-      
-        return (
-          <div className='row'>{
-          row.split('').map(item => {
-            console.log(item)
-            if(item === "."){
-              return (
-                <div className='empty'>.</div>
-              )
-            } else if( item === '#'){
-              return (
-                <div className='wall'>#</div>
-              )
-            } else if( item === '+'){
-              return (
-                <div className='lava'>+</div>
-              )
-            } else if( item === '@'){
-              return (
-                <div className='player'>@</div>
-              )
-            } else if( item === 'o'){
-              return (
-                <div className='coin'>o</div>
-              )
-            } else if( item === '='){
-              return (
-                <div className='lava'>=</div>
-              )
-            }else if( item === 'v'){
-              return (
-                <div className='lava'>=</div>
-              )
-            }
-            return item
-          })
-          
-        }</div>
-
-        )
-      })}
-      </div>
-     
+      <RenderLevel />
     </div>
   );
-}
+};
 
 export default App;
